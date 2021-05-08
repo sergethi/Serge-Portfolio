24484ac3a198a84708f9d6b64e66e15bce5545ca
SERGE <nsergearistide@gmail.com>
2021-05-08 00:53:02 -0400

    Updates

---
+++ precache-manifest.39280d7fcd0133e0f4683d6a77abb982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b2cd7e77f88d3f3a06bab40e75aff3",
+    "url": "/Serge-Portfolio/index.html"
+  },
+  {
+    "revision": "e923faef246f35facca7",
+    "url": "/Serge-Portfolio/static/css/main.8744f824.chunk.css"
+  },
+  {
+    "revision": "18edac9ddc168fab4332",
+    "url": "/Serge-Portfolio/static/js/2.05fefb38.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Serge-Portfolio/static/js/2.05fefb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e923faef246f35facca7",
+    "url": "/Serge-Portfolio/static/js/main.df5a7a35.chunk.js"
+  },
+  {
+    "revision": "a4210929b7d55764aa5c",
+    "url": "/Serge-Portfolio/static/js/runtime-main.8683c637.js"
+  }
+]);
