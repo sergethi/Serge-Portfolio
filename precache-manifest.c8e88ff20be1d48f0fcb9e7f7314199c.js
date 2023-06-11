79b4989888bbec32e404b79890e13a3341e77e45
serge <nsergearistide@gmail.com>
2023-06-11 03:34:04 -0400

    Updates

---
+++ precache-manifest.c8e88ff20be1d48f0fcb9e7f7314199c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddef2d46aa45dfdf53f0",
+    "url": "/Serge-Portfolio/static/css/main.5f26dddf.chunk.css"
+  },
+  {
+    "revision": "ddef2d46aa45dfdf53f0",
+    "url": "/Serge-Portfolio/static/js/main.e9d5cdff.chunk.js"
+  },
+  {
+    "revision": "9a1026c8d4f9ced06ec1",
+    "url": "/Serge-Portfolio/static/js/runtime~main.63b33ee8.js"
+  },
+  {
+    "revision": "ffefe3a978d6650f1e4a",
+    "url": "/Serge-Portfolio/static/js/2.1ec97ce1.chunk.js"
+  },
+  {
+    "revision": "c65077872acaa470a9299933709796a6",
+    "url": "/Serge-Portfolio/index.html"
+  }
+];
